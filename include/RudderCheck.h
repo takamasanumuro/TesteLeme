9d8f768d436347b3907c91613323946edd64ac0d
adrianowerneck <adrianowerneck@id.uff.br>
2023-01-04 15:15:00 -0300

    Teste Radio + PID

--- include/RudderCheck.h
+++ include/RudderCheck.h
@@ -3,6 +3,8 @@
 int ReadRudder();
 void MoveRudder();
 int GetSerialAngle();
+int GetRadioAngle(int16_t pixhawk_channel);
+void PrintRadioAngle(uint32_t print_delay, int16_t pixhawk_channel);
 
 float PID_Proportional(float present_error);
 float PID_Integral(float present_error);
